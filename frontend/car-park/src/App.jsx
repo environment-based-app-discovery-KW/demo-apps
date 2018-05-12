740bdb1b1c8b17d0a89beaa57a3a7e15ccbef4e5
Ke Wang <453587854@qq.com>
2018-05-12 14:10:20 +0800

    停车场缴费界面

--- frontend/car-park/src/App.jsx
+++ frontend/car-park/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Icon from "./assets/icon.svg";
 
 class App extends Component {
 
@@ -9,6 +10,22 @@ class App extends Component {
 
   render() {
     return <div>
+      <div className="banner">
+        停车场缴费
+      </div>
+      <div className="icon-wrapper">
+        <Icon className="app-icon"/>
+      </div>
+      <div className="plaza-name">
+        商业广场 - 停车场
+      </div>
+      <div className="actions">
+        <input className="input" type="text" placeholder="请输入车牌号以查询.."/>
+        <div className="button-bar">
+          <a class="button is-primary">付车费</a>
+          <a class="button is-success">寻车导航</a>
+        </div>
+      </div>
     </div>
   }
 }
--- frontend/car-park/src/index.js
+++ frontend/car-park/src/index.js
@@ -1,6 +1,6 @@
 import "./lib/sys";
 import "./lib/bulma.min.css"
-import "./style.css"
+import "./style.less"
 const _ = require('lodash');
 const ReactDOM = require("react-dom");
 const React = require("react");
